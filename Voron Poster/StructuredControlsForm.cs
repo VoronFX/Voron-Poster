e4e7358a94e3768f3afc0abee7077a1d905e7745
Voronin Igor <Voron.exe@gmail.com>
2014-07-06 20:46:49 +0300

    Still no luck with structuring controls for form designer

---
+++ Voron Poster/StructuredControlsForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Voron_Poster
+{
+    public partial class StructuredControlsForm : Form
+    {
+        public ScriptsControls Scripts;
+        public partial struct ScriptsControls
+        {
+            public string OpenedScript;
+            // global::Voron_Poster.MainForm MainForm;
+            // public ScriptsControls(global::Voron_Poster.MainForm ParentForm) : this() { MainForm = ParentForm; }
+            public ScintillaNET.Scintilla CodeEditor;
+            public System.Windows.Forms.TextBox CodeBox;
+            public System.Windows.Forms.TextBox NameBox;
+            public System.Windows.Forms.Button AcceptButton;
+            public System.Windows.Forms.ListBox ListBox;
+            public System.Windows.Forms.Button DeleteButton;
+            public System.Windows.Forms.Button NewButton;
+            public System.Windows.Forms.Button SaveButton;
+
+            public System.Windows.Forms.Button RunButton;
+
+            public System.Windows.Forms.Splitter ListSplitter;
+            public System.Windows.Forms.Panel ListPanel;
+            public System.Windows.Forms.Splitter TestSplitter;
+            public System.Windows.Forms.Panel TestPanel1;
+            public System.Windows.Forms.Panel TestPanel2;
+
+            public System.Windows.Forms.TabPage Tab;
+            public System.Windows.Forms.TabControl Tabs;
+            public System.Windows.Forms.TabPage CodeTab;
+            //public CodeControls Test;
+            //public partial struct CodeControls
+            //{ 
+
+            //}
+
+            //public TestControls Test;
+            //public partial struct TestControls
+            //{
+            public System.Windows.Forms.TextBox ResultBox;
+            public System.Windows.Forms.TextBox SubjectBox;
+            public System.Windows.Forms.TabPage TestTab;
+            public System.Windows.Forms.Label StatusLabel;
+            public System.Windows.Forms.TextBox MessageBox;
+            //}
+        }
+    }
+}
